b4e1e5c239eb0044f66ad5dc1f6c78befde79b35
Jonathan S. Winther <jonathanswinther@gmail.com>
2023-04-09 12:17:28 +0200

    ex

---
+++ ConsoleApp/DataStructures/ArrayExtensions.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp.DataStructures
+{
+    public static class ArrayExtensions
+    {
+        public static IEnumerable<int> GetViewBetween(this int[] array, int x, int y)
+        {
+            List<int> occs = new();
+            //var mid = Array.Find(array, element => x <= element && element <= y);
+            
+            int lo = 0;
+            int hi = array.Length - 1;
+            int mid = 0;
+            while (lo <= hi)
+            {
+                mid = lo + (hi - lo) / 2;
+           
+                if (array[mid] > y)
+                {
+                    hi = mid - 1;
+                }
+                else if (array[mid] < x)
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            // TODO this is not sorted..
+            int i = mid, j = mid - 1;
+            while (i < array.Length && array[i] <= y)
+            {
+                occs.Add(array[i]);
+                i++;
+            }
+
+            while (j > - 1 && array[j] >= x)
+            {
+                occs.Add(array[j]);
+                j--;
+            }
+            return occs;
+        }
+    }
+}
--- ConsoleApp/DataStructures/SuffixArray_V5.cs
+++ ConsoleApp/DataStructures/SuffixArray_V5.cs
@@ -49,48 +49,4 @@ namespace ConsoleApp.DataStructures
             return occs;
         }
     }
-
-    public static class ArrayExtensions
-    {
-        public static IEnumerable<int> GetViewBetween(this int[] array, int x, int y)
-        {
-            LinkedList<int> occs = new();
-            //var mid = Array.Find(array, element => x <= element && element <= y);
-            
-            int lo = 0;
-            int hi = array.Length - 1;
-            int mid = 0;
-            while (lo <= hi)
-            {
-                mid = lo + (hi - lo) / 2;
-           
-                if (array[mid] > y)
-                {
-                    hi = mid - 1;
-                }
-                else if (array[mid] < x)
-                {
-                    lo = mid + 1;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            // TODO this is not sorted..
-            int i = mid, j = mid - 1;
-            while (i < array.Length && array[i] <= y)
-            {
-                occs.AddLast(array[i]);
-                i++;
-            }
-
-            while (j > - 1 && array[j] >= x)
-            {
-                occs.AddLast(array[j]);
-                j--;
-            }
-            return occs;
-        }
-    }
 }
