069041f38483b17aa541d2c1bda8c24a148d3d55
Jonathan S. Winther <jonathanswinther@gmail.com>
2023-05-22 12:50:22 +0200

    v0

--- ConsoleApp/DataStructures/Count/CountDataStructure.cs
+++ ConsoleApp/DataStructures/Count/CountDataStructure.cs
@@ -8,8 +8,14 @@ namespace ConsoleApp.DataStructures.Count
 {
     public abstract class CountDataStructure
     {
+        protected int x;
+        protected int y_min;
+        protected int y_max;
         protected CountDataStructure(string str, int x, int ymin, int ymax)
         {
+            this.x = x;
+            y_min = ymin;
+            y_max = ymax;
         }
         public abstract int Matches(string pattern);
         public abstract int MatchesFixed(string pattern1, string pattern2);
